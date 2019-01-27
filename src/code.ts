cf1ae00ca81f647cf7ea4f5c4e81d34193a465c1
Eakan Gopalakrishnan <me_theonlyone@hotmail.com>
2019-01-27 19:51:35 +0000

    Fixed lint errors
    learning to write good typescript with tslint is good.

--- src/code.ts
+++ src/code.ts
@@ -1,15 +1,15 @@
 import { Observable } from "rxjs";
 
 // this is just one of a thousand ways to create an observable
-var observable = Observable.create((observer:any) => {
+var observable: Observable<string> = Observable.create((observer:any) => {
     observer.next("Hey guys!");
 });
 
 observable.subscribe((x:any) => addItem(x));
 
-function addItem(val:any) {
-    var node = document.createElement("li");
-    var textnode = document.createTextNode(val);
+function addItem(val:any): void {
+    var node: HTMLLIElement = document.createElement("li");
+    var textnode: Text = document.createTextNode(val);
     node.appendChild(textnode);
     document.getElementById("output").appendChild(node);
 }
