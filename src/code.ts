6884bf613dd9b908fd3288f50c77cdee5ca0ff8f
Eakan Gopalakrishnan <me_theonlyone@hotmail.com>
2019-01-27 19:33:35 +0000

    Use arrow functions
    lambdas make typing functions quicker.
    obviously you wouldn't type a lot of keywords.

--- src/code.ts
+++ src/code.ts
@@ -1,6 +1,6 @@
 import { Observable } from "rxjs";
 
 // this is just one of a thousand ways to create an observable
-var observable = Observable.create(function subscribe(observer:any){
+var observable = Observable.create((observer:any) => {
     observer.next("Hey guys!");
 });
