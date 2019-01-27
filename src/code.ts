98769e9cfa94b6918a8dcfa9725cc05270ebcdfa
Eakan Gopalakrishnan <me_theonlyone@hotmail.com>
2019-01-27 20:00:11 +0000

    Add error and complete methods to the observer
    Observer is generally partially defined by just having the next method
    defined. However, now that we are learning, let us implement the error
    and complete methods too.

--- src/code.ts
+++ src/code.ts
@@ -1,13 +1,24 @@
 import { Observable } from "rxjs";
 
 // this is just one of a thousand ways to create an observable
-var observable: Observable<string> = Observable.create((observer:any) => {
-    observer.next("Hey guys!");
+var observable: Observable<string> = Observable.create((observer: any) => {
+    try {
+        observer.next("Hey guys!");
+        observer.next("How are you?");
+        observer.complete();
+        observer.next("This will never be sent.");
+    } catch (err) {
+        observer.error(err);
+    }
 });
 
-observable.subscribe((x:any) => addItem(x));
+observable.subscribe(
+    (x: any) => addItem(x),
+    (error: any) => addItem(error),
+    () => addItem("completed")
+);
 
-function addItem(val:any): void {
+function addItem(val: any): void {
     var node: HTMLLIElement = document.createElement("li");
     var textnode: Text = document.createTextNode(val);
     node.appendChild(textnode);
