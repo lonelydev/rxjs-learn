f1acfcc29a48fa9f23cb04424ecc8bf9517f461d
Eakan Gopalakrishnan <me_theonlyone@hotmail.com>
2019-01-27 19:43:04 +0000

    Output to webpage instead of console
    Lets grab hold of the output element on the webpage
    and print our demo observable stuff there.

--- src/code.ts
+++ src/code.ts
@@ -4,3 +4,12 @@ import { Observable } from "rxjs";
 var observable = Observable.create((observer:any) => {
     observer.next("Hey guys!");
 });
+
+observable.subscribe((x:any) => addItem(x));
+
+function addItem(val:any) {
+    var node = document.createElement("li");
+    var textnode = document.createTextNode(val);
+    node.appendChild(textnode);
+    document.getElementById("output").appendChild(node);
+}
