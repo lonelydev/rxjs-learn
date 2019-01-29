bf4c36f75fc540c5bcbab9fe9ccdd34a88b13083
Eakan Gopalakrishnan <me_theonlyone@hotmail.com>
2019-01-29 19:54:32 +0000

    Behavior Subjects - note the american spelling
    A subject which when subscribed to receives the most recently emitted value.
    Thus a new BehaviorSubject has to be initialised with a value.
    Like a seed value.

--- src/code.ts
+++ src/code.ts
@@ -1,9 +1,9 @@
-import { Subject, Subscription } from "rxjs";
+import { BehaviorSubject, Subscription } from "rxjs";
 
 /***
  * Subject is an observable that is also able to emit a value
  */
-var subject1: Subject<any> = new Subject();
+var subject1: BehaviorSubject<any> = new BehaviorSubject("First");
 
 subject1.subscribe(
     data => addItem("Observer1:" + data),
@@ -12,7 +12,7 @@ subject1.subscribe(
 );
 
 subject1.next("The first thing has been sent");
-
+subject1.next("...Observer 2 is about to subscribe.");
 /**
  * observer2 will not receive first thing as it is created 
  * after the first thing was sent.
